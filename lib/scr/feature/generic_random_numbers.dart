c2e6ee3a4199795dfafd3c378aa7af7d0e86df36
stanislav spasonov <s.spasonov@itransition.com>
2023-03-07 17:09:38 +0400

    add

---
+++ lib/scr/feature/generic_random_numbers.dart
@@ -0,0 +1,6 @@
+import 'dart:math';
+
+var romas = 1;
+void getrandomnumber() {
+  romas = Random().nextInt(8) + 1;
+}
--- lib/scr/feature/magic_ball_page.dart
+++ lib/scr/feature/magic_ball_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:magic_ball_learn/scr/feature/generic_random_numbers.dart';
 
 class MagicBallPage extends StatefulWidget {
   const MagicBallPage({Key? key}) : super(key: key);
@@ -9,11 +10,39 @@ class MagicBallPage extends StatefulWidget {
 
 class _MagicBallPageState extends State<MagicBallPage> {
   @override
+  var roma = 1;
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("MagicBall"),
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        centerTitle: true,
+        title: Text('Ask me Anything'),
       ),
+      backgroundColor: Colors.deepPurple,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              ' I am Diana',
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          ],
+        ),
+      ),
+      // body: GestureDetector(
+      //   onTap: () {
+      //     print('i am tap');
+      //     getrandomnumber();
+      //     roma = romas;
+      //     setState(() {});
+      //   },
+      //   child: Center(
+      //     child: Image.asset('assets/ball$roma.png'),
+      //   ),
+      // ),
     );
   }
 }
